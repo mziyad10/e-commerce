8d7f60a0535ab4785d28945317101f9e11541414
mziyad10 <mvsiyad10@gmail.com>
2025-07-07 16:26:37 +0530

    completed list product css and functioning

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,18 @@ const ListProduct = () => {
       fetchInfo();
     },[])
 
+    const remove_product=async (id)=>{
+      await fetch('http://localhost:4000/removeproduct',{
+        method:'POST',
+        headers:{
+          Accept:'application/json',
+          'content-type':'application/json',
+        },
+        body:JSON.stringify({id:id})
+      })
+      await fetchInfo();
+    }
+
 
   return (
     <div className='list-product'>
@@ -31,14 +43,15 @@ const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index)=>{
-          return <div key={index} className="listproduct-format-main listproduct-format">
+          return <><div key={index} className="listproduct-format-main listproduct-format">
               <img src={product.image} alt="" className="listproduct-product-icon" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img className='listproduct-remove-icon' src={cross_icon} alt="" />
+              <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
           </div>
+          <hr /></>
         })}
       </div>
     </div>
